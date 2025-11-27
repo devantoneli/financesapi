e0f061a7d0fa446a537246e15ac797af9fe8fef6
Guilherme Souto <devantoneli@users.noreply.github.com>
2025-11-26 23:50:28 -0300

    Recriando a tela lançamento junto com ajustes

--- src/main/java/com/grecfinances/controller/LancamentosController.java
+++ src/main/java/com/grecfinances/controller/LancamentosController.java
@@ -19,69 +19,51 @@ public class LancamentosController {
         
         TransacaoModel t1 = new TransacaoModel();
         t1.setId(1L);
-        t1.setDescricao("Uber para Casa");
-        t1.setCategoria("Dia a Dia");
-        t1.setValor(10.00f);
-        t1.setData(LocalDate.of(2025, 11, 24));
+        t1.setDescricao("Freelancer - Projeto Web");
+        t1.setCategoria("Trabalho");
+        t1.setValor(new BigDecimal("1500.00"));
+        t1.setData(LocalDate.of(2025, 11, 25));
         t1.setTipo("Receita");
         lancamentos.add(t1);
 
         TransacaoModel t2 = new TransacaoModel();
         t2.setId(2L);
-        t2.setDescricao("Uber para Casa");
-        t2.setCategoria("Dia a Dia");
-        t2.setValor(10.00f);
+        t2.setDescricao("Aluguel - Apartamento");
+        t2.setCategoria("Moradia");
+        t2.setValor(new BigDecimal("1200.00"));
         t2.setData(LocalDate.of(2025, 11, 24));
         t2.setTipo("Despesa");
         lancamentos.add(t2);
 
         TransacaoModel t3 = new TransacaoModel();
         t3.setId(3L);
-        t3.setDescricao("Uber para Casa");
-        t3.setCategoria("Dia a Dia");
-        t3.setValor(10.00f);
+        t3.setDescricao("Supermercado - Alimentos");
+        t3.setCategoria("Alimentação");
+        t3.setValor(new BigDecimal("320.50"));
         t3.setData(LocalDate.of(2025, 11, 24));
-        t3.setTipo("Receita");
+        t3.setTipo("Despesa");
         lancamentos.add(t3);
 
         TransacaoModel t4 = new TransacaoModel();
         t4.setId(4L);
-        t4.setDescricao("Uber para Casa");
-        t4.setCategoria("Dia a Dia");
-        t4.setValor(10.00f);
-        t4.setData(LocalDate.of(2025, 11, 24));
+        t4.setDescricao("Salário");
+        t4.setCategoria("Renda");
+        t4.setValor(new BigDecimal("5000.00"));
+        t4.setData(LocalDate.of(2025, 11, 23));
         t4.setTipo("Receita");
         lancamentos.add(t4);
 
         TransacaoModel t5 = new TransacaoModel();
         t5.setId(5L);
-        t5.setDescricao("Uber para Casa");
-        t5.setCategoria("Dia a Dia");
-        t5.setValor(10.00f);
-        t5.setData(LocalDate.of(2025, 11, 24));
-        t5.setTipo("Receita");
+        t5.setDescricao("Academia - Mensalidade");
+        t5.setCategoria("Saúde");
+        t5.setValor(new BigDecimal("150.00"));
+        t5.setData(LocalDate.of(2025, 11, 22));
+        t5.setTipo("Despesa");
         lancamentos.add(t5);
 
-        TransacaoModel t6 = new TransacaoModel();
-        t6.setId(6L);
-        t6.setDescricao("Uber para Casa");
-        t6.setCategoria("Dia a Dia");
-        t6.setValor(10.00f);
-        t6.setData(LocalDate.of(2025, 11, 24));
-        t6.setTipo("Receita");
-        lancamentos.add(t6);
-
-        TransacaoModel t7 = new TransacaoModel();
-        t7.setId(7L);
-        t7.setDescricao("Uber para Casa");
-        t7.setCategoria("Dia a Dia");
-        t7.setValor(10.00f);
-        t7.setData(LocalDate.of(2025, 11, 24));
-        t7.setTipo("Receita");
-        lancamentos.add(t7);
-
         model.addAttribute("lancamentos", lancamentos);
-        model.addAttribute("total", 150);
+        model.addAttribute("total", lancamentos.size());
         return "lancamentos";
     }
 }
